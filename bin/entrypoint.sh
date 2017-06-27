749a42cbae2610bb13a38b625a7dc878d022e44b
Marcio Godoi <souzagodoi@gmail.com>
2017-06-27 05:05:37 -0300

    Updating shell references to run a entrypoint script

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -15,10 +15,11 @@ sed -i "s/CLUSTER_NAME/$CLUSTER_NAME/
   s/LOCK_MEMORY/$LOCK_MEMORY/
   s/NETWORK_HOST/$NETWORK_HOST/
   s/UNICAST_HOSTS/$UNICAST_HOSTS/
+  s/DISCOVERY_HOSTS/$DISCOVERY_HOSTS/
   s/MINIMUM_MASTER_NODES/$MINIMUM_MASTER_NODES/" ./config/elasticsearch.yml
 
 #echo -e "Starting Elasticsearch $ELASTICSEARCH_VERSION"
-#exec /opt/elasticsearch/bin/elasticsearch
+exec /opt/elasticsearch/bin/elasticsearch
 
 #echo -e "Starting Kibana... "
 #exec /opt/kibana/bin/kibana
@@ -26,8 +27,8 @@ sed -i "s/CLUSTER_NAME/$CLUSTER_NAME/
 #echo -e "Starting Logstash... "
 #exec /opt/logstash/bin/logstash -f /opt/logstash/conf/logstash.conf
 
-while true
-do
-    echo "Press [CTRL+C] to stop..."
-    sleep 1
-done
+#while true
+#do
+#    echo "Press [CTRL+C] to stop..."
+#    sleep 1
+#done
